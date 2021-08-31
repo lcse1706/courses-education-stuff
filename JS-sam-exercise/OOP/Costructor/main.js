745947161ea2746044ed674259c0b6ab52c889a3
Lukas <lcse.2001@gmail.com>
2021-08-31 21:11:47 +0200

    prototype cd

--- JS-sam-exercise/OOP/Costructor/main.js
+++ JS-sam-exercise/OOP/Costructor/main.js
@@ -21,3 +21,36 @@ Person.prototype.addChildren = function(name) {
 const philip = new Person('Philip', 24);
 const monica = new Person('Monica', 28);
 
+//Prototype 2
+
+const Citizen = function (country, citizenship) {
+    this.country = country;
+    this.citizenship = citizenship;
+}
+
+Citizen.prototype.changeCitizenship = function(citizenship) {
+    this.citizenship = citizenship
+}
+
+const polish = new Citizen ("poland", "polish");
+const svensk = new Citizen ("sweden", "swedish");
+
+//Prototype 3
+
+//prototype chain
+
+svensk.__proto__ // Citizen
+svensk.__proto__.__proto__ // Object
+svensk.__proto__.__proto__.__proto__ // Null
+
+Object.prototype.age = 20;
+
+svensk.age //20
+
+//instanceof
+
+svensk instanceof Citizen //true
+svensk instanceof Object // true
+svensk instanceof Person // false
+
+// Object.getPrototypeOf
