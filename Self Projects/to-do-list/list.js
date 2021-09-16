ce0edd6d15d3407ec5a866bd10606795c6c7ad32
Lukas <lcse.2001@gmail.com>
2021-09-16 20:11:45 +0200

    delete and renderList - not working

--- Self Projects/to-do-list/list.js
+++ Self Projects/to-do-list/list.js
@@ -3,12 +3,29 @@ class List {
 
     }
 
-    addTask(toDoElement, list){
+    addTask(toDoList, toDoElement, list){
             
         const task = document.createElement('li');
-        task.innerHTML = toDoElement.value;
+        task.innerHTML = toDoElement.value + '<button>delete</button>';
+        toDoList.push(task);
         list.appendChild(task);
         toDoElement.value = "";
         
     }
+
+    removeTask(){
+        const index = e.target.parentNode.dataset.key;
+        toDoList.splice(index, 1); 
+    }
+
+
+    renderList(list, toDoList){
+        list.textContent = "";
+        toDoList.forEach((toDoElement, key) => {
+            toDoElement.dataset.key = key;
+            list.appendChild(toDoElement);
+        })
+    }
+
+    
 }
--- Self Projects/to-do-list/main.js
+++ Self Projects/to-do-list/main.js
@@ -1,12 +1,21 @@
+const toDoList = [];
+
 const btn = document.getElementById('confirm');
 const input = document.querySelector('.task')
 const list = document.querySelector('ol');
+const liElements = document.querySelectorAll('li');
 
 this.tasks = new List();
 
 btn.addEventListener('click', () => {
     
-    tasks.addTask(input, list);
+    tasks.addTask(toDoList, input, list);
 })
 
+
+liElements.forEach((element) => {
+    element.addEventListener('click', () => {
+        console.log('adsff')
+    })
+})   
 
