3d45991f2d3078a159b79a90e6c09703742e7d5d
Lukas <lcse.2001@gmail.com>
2021-09-21 12:45:03 +0200

    Add task

--- Self Projects/to-do-list/main.js
+++ Self Projects/to-do-list/main.js
@@ -1,4 +1,5 @@
 // Task Class: Represens Task
+const taskList = [];
 
 class Task {
     constructor(title){
@@ -22,20 +23,27 @@ class UI {
         }
        ];
 
-       const tasks = savedTask;
-
-       tasks.forEach((task) => UI.addTaskToList(task));
+       savedTask.forEach((task) => UI.addTaskToList(task));
+       savedTask.forEach((task) => taskList.push(task));
     }
 
     static addTaskToList(task){
         
         const list = document.getElementById('task-list');
         const singleTask = document.createElement('li');
-        singleTask.innerHTML = `${task.title} <a href="#" class = "btn btn-danger btn-sm delete">X</a>`;
+        singleTask.innerHTML = `${task.title} <a href="#" class="btn btn-danger btn-sm delete">X</a>`;
         
         list.appendChild(singleTask);
+    }
 
+    static getTask() {
+        const input = document.querySelector('.form-control').value;
+        const task = new Task (input);
+        UI.addTaskToList(task);
+        taskList.push(task);
+    }
 
+    
     }
 }
 
@@ -45,4 +53,8 @@ document.addEventListener('DOMContentLoaded', UI.displayList);
 
 // Event: Add task
 
+const addBtn = document.querySelector('.add');
+addBtn.addEventListener('click', UI.getTask);
+
 // Event: Remove Task
+
