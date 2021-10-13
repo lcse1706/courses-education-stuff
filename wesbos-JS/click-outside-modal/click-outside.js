55748be13f260a685e7d6eb1ff45f824d265a147
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-10-13 19:19:41 +0200

    full working gallery without classes

--- wesbos-JS/click-outside-modal/click-outside.js
+++ wesbos-JS/click-outside-modal/click-outside.js
@@ -1,48 +1,48 @@
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('button');
 
 const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
 
 function handleButton(e) {
-    const button = e.currentTarget;
-    const card = button.closest('.card');
+  const button = e.currentTarget;
+  const card = button.closest('.card');
 
-    // grab the image source !!!
-    
-    const imgSrc = card.querySelector('img').src;
-    const desc = card.dataset.description;
-    const name = card.querySelector('h2').textContent;
+  // grab the image source !!!
 
-    // populate the modal with ne info
-    modalInner.innerHTML=`
-     <img width='600' height='600' src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+
+  // populate the modal with ne info
+  modalInner.innerHTML = `
+     <img width='600' height='600' src="${imgSrc.replace(
+       '200',
+       '600'
+     )}" alt="${name}"/>
      <p>${desc}</p>
     `;
-    //show the modal
-    modalOuter.classList.add('open');
-
-    console.log("dziala")
+  //show the modal
+  modalOuter.classList.add('open');
 }
 
-
-buttons.forEach( button => {
-    button.addEventListener('click', handleButton);
-})
-
-function closeModal() {
-    modalOuter.classList.remove('open');
-}
-
-modalOuter.addEventListener('click', function(e){
-    const isOutside = !e.target.closest('.modal-inner');
-
-    if (isOutside) {
-        closeModal();
-    }
+buttons.forEach((button) => {
+  button.addEventListener('click', handleButton);
 });
 
-window.addEventListener('keyup', e => {
-    if(e.key === 'Escape'){
-        closeModal()
-    }
-})
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function (e) {
+  const isOutside = e.target.closest('.modal-inner');
+
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
---
+++ wesbos-JS/gallery/gallery.js
@@ -0,0 +1,86 @@
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error('No gallery found');
+  }
+  // get all elemnts we need
+  const images = Array.from(gallery.querySelectorAll('img'));
+  const modal = document.querySelector('.modal');
+  const prevButton = modal.querySelector('.prev');
+  const nextButton = modal.querySelector('.next');
+  let currentImage;
+
+  function openModal() {
+    console.info('Opening Modal...');
+
+    if (modal.matches('.open')) {
+      console.info('Modal already open');
+      return;
+    }
+    modal.classList.add('open');
+
+    window.addEventListener('keyup', handleKeyUp);
+    nextButton.addEventListener('click', showNextImage);
+    prevButton.addEventListener('click', showPrevImage);
+  }
+
+  function closeModal() {
+    modal.classList.remove('open');
+
+    window.removeEventListener('keyup', handleKeyUp);
+    nextButton.removeEventListener('click', showNextImage);
+    prevButton.removeEventListener('click', showPrevImage);
+  }
+
+  function handleClickOutside(e) {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  }
+
+  function handleKeyUp(e) {
+    if (e.key === 'Escape') return closeModal();
+    if (e.key === 'ArrowRight') return showNextImage();
+    if (e.key === 'ArrowLeft') return showPrevImage();
+  }
+
+  function showNextImage() {
+    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+  }
+  function showPrevImage() {
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+  }
+
+  function showImage(el) {
+    if (!el) {
+      console.info('no image to show');
+      return;
+    }
+    //update the modal with this info
+    console.log(el);
+    modal.querySelector('img').src = el.src;
+    modal.querySelector('h2').textContent = el.title;
+    modal.querySelector('figure p').textContent = el.dataset.description;
+    currentImage = el;
+    openModal();
+  }
+
+  function handleImageClick(e) {
+    showImage(e.currentTarget);
+  }
+
+  images.forEach((image) => image.addEventListener('click', handleImageClick));
+
+  images.forEach((image) =>
+    image.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        showImage(e.currentTarget);
+      }
+    })
+  );
+
+  modal.addEventListener('click', handleClickOutside);
+}
+
+//Use them on a page
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
