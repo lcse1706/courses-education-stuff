63e9264f51718d33790cc4ae959388c5c4b4c075
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-10-21 10:26:03 +0200

    eamil validation

--- BradTraversy-20-Vanilla-JS/form-validator/script.js
+++ BradTraversy-20-Vanilla-JS/form-validator/script.js
@@ -12,6 +12,31 @@ const errMessage = document.querySelector('small');
 
 // validate email
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!(re.test(String(email.value).toLowerCase()))) {
+        (email.parentElement.classList.add('error'));
+    }
+    
+    
+    
+
+    // return re.test(String(email.value).toLowerCase());
+}
+
+// red green class
+
+function isValid(item, boolean) {
+    
+    if(boolean == 'true'){
+        item.parentElement.classList.add('succes');
+    } else {
+        item.parentElement.classList.add('error');
+    }
+
+}
+
 //create error message
 
 function createError(item, text){
@@ -20,20 +45,22 @@ function createError(item, text){
 }
 
 function capitalizeElement(input) {
-    const cap = input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    const cap = input.charAt(0).toUpperCase() + input.slice(1);
     return cap;
 }
 
 //check inputs, add success or error
 function checkInput(input){
     input.forEach((item) => {
+        
         const formControl = item.parentElement;
+        
         formControl.className = 'form-control';
-        if(!!item.value) {
+        if(!!item.value.trim()) {
             formControl.classList.add('success');
         } else {
             formControl.classList.add('error');
-            formControl.querySelector('small').innerHTML = createError(capitalizeElement(item), 'is not valid')
+            formControl.querySelector('small').innerHTML = createError(capitalizeElement(item.id), 'is not valid')
         }
     })
 }
@@ -43,7 +70,9 @@ function checkInput(input){
 function handleSubmit(e){
     e.preventDefault();
 
-    checkInput([username, email, password, password2]) 
+    checkInput([username, email, password, password2]);
+    validateEmail(email);
+    
 
 }
 
