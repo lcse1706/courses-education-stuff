9479661a4c78b0208dae26dbfe530deba944d323
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-12-05 21:49:48 +0100

    keeping practice components

--- React-Max/my-first-app/src/components/ExpenseItem.js
+++ React-Max/my-first-app/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from './Card';
+
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
