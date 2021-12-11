85bef9b63bb019bcff4db09f371c68f56e843c00
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-12-12 00:22:48 +0100

    refactored to functional approach

--- React/BradTraversy-Udemy/first-init/src/components/layout/Navbar.js
+++ React/BradTraversy-Udemy/first-init/src/components/layout/Navbar.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 // sc - impt
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github',
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  //Set the prop types
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github',
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+//Set the prop types
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
