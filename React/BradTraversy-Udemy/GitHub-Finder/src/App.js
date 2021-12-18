e1ba48e31b999f3169a6ec6fe2a0e17f24c2c3c5
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-12-18 22:46:34 +0100

    Alert notification

--- React/BradTraversy-Udemy/GitHub-Finder/src/App.js
+++ React/BradTraversy-Udemy/GitHub-Finder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -36,16 +38,30 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (message, type) => {
+    this.setState({
+      alert: { message, type },
+    });
+    setTimeout(() => {
+      this.setState({
+        alert: null,
+      });
+    }, 5000);
+  };
+
   render() {
+    console.log(this.state.alert);
     const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
