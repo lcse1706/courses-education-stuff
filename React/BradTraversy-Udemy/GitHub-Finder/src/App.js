99a5760f050b01d0e85311de70d86519f84f1aaa
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-12-19 00:47:30 +0100

    working on single user route

--- React/BradTraversy-Udemy/GitHub-Finder/src/App.js
+++ React/BradTraversy-Udemy/GitHub-Finder/src/App.js
@@ -2,15 +2,18 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import { About } from './components/pages/About';
+
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -31,9 +34,17 @@ class App extends Component {
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    console.log(res.data);
-
     this.setState({ users: res.data.items, loading: false });
+  };
+
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+    console.log('This is our user: ', this.state.user);
   };
 
   clearUsers = () => {
@@ -53,7 +64,7 @@ class App extends Component {
 
   render() {
     console.log(this.state.alert);
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -77,6 +88,12 @@ class App extends Component {
                 }
               />
               <Route path='/about' element={<About />} />
+              <Route
+                path='/user/:login'
+                element={
+                  <User getUser={this.getUser} user={user} loading={loading} />
+                }
+              />
             </Routes>
           </div>
         </div>
--- React/BradTraversy-Udemy/GitHub-Finder/src/components/users/UserItem.js
+++ React/BradTraversy-Udemy/GitHub-Finder/src/components/users/UserItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 //possible to destructure straightly in props
-const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+const UserItem = ({ user: { avatar_url, login } }) => {
   return (
     <div className='card text-center'>
       <img
@@ -13,9 +14,9 @@ const UserItem = ({ user: { avatar_url, login, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
