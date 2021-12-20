45efc55b4ecfb8474932e212817dd398d4fa074f
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-12-20 20:43:26 +0100

    7

---
+++ Przeprogramowani-JS-bootcamp/exercises/7_high-order-function.js
@@ -0,0 +1,62 @@
+// *
+// * Opanuj JavaScript - Przeprogramowani.pl
+// * I. Fundamenty języka JavaScript
+// *
+// * Ćwiczenie 6 - "W dwóch krokach"
+// */
+
+/*
+ * Cel zadania
+ *------------
+ * Zaimplementuj funkcję, która zwróci kolejną funkcję dodającą do przekazywanego
+ * parametru zarejestrowany wcześniej przedrostek.
+ *
+ *
+ * Przykład:
+ * const greeting = withPrefix('Witaj, ');
+ * greeting('Janek'); // => 'Witaj, Janek'
+ * greeting('Tomek'); // => 'Witaj, Tomek'
+ *
+ * const goodbye = withPrefix('Żegnaj, ');
+ * goodbye('Janek'); // => 'Żegnaj, Janek'
+ * goodbye('Tomek'); // => 'Żegnaj, Tomek'
+ *
+ * Utworzona funkcja to tzw. funkcja wyższego rzędu.
+ */
+
+/*
+ * Punkty dodatkowe
+ *-----------------
+ * Funkcja powinna weryfikować, czy przekazany parametr jest typu string. Jeśli parametr nie
+ * spełnia tego warunku, funkcja powinna rzucić wyjątek.
+ */
+
+function withPrefix(prefix) {
+  if (typeof prefix !== 'string') throw new Error('This is not a string value');
+
+  const addPrefix = name => {
+    return `${prefix}${name}`;
+  };
+  return addPrefix;
+}
+
+/* Weryfikacja */
+
+function verify(input, goal) {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
+
+const course = withPrefix('I ty Opanujesz JavaScript, ');
+
+console.log(course('Łukasz'));
+
+try {
+  verify(course('Marta'), 'I ty Opanujesz JavaScript, Marta');
+  verify(course('Janek'), 'I ty Opanujesz JavaScript, Janek');
+} catch {
+  console.log('Niestety :(');
+}
