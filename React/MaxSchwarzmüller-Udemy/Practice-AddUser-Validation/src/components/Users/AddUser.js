b1a57f1dd30b6439c915870e6ea3a52f9c98e3a2
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-04 19:31:25 +0100

    Fetching data from iputs,

--- React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/components/Users/AddUser.js
+++ React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/components/Users/AddUser.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 import classes from './AddUser.module.css';
+// import { useState } from 'react';
 
 const AddUser = props => {
+  const [enteredUsername, setUsername] = useState('');
+  const [enteredAge, setAge] = useState('');
+
   const AddUserHandler = e => {
     e.preventDefault();
+
+    if (enteredUsername.trim() === 0 || enteredAge.trim() === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge);
+    setUsername('');
+    setAge('');
+  };
+
+  const changeUsernameHandler = e => {
+    setUsername(e.target.value);
+  };
+
+  const changeAgeHandler = e => {
+    setAge(e.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={AddUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          value={enteredUsername}
+          onChange={changeUsernameHandler}
+        />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' />
+        <input
+          id='age'
+          type='number'
+          value={enteredAge}
+          onChange={changeAgeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
