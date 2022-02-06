6826239ff3769ea90b11cda05072a41072ae9c2a
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-06 17:56:48 +0100

    Display added users

--- React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/App.js
+++ React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUsersListHandler = (userName, userAge) => {
+    setUsersList(prevUsersList => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+    console.log('addUsersListHandler');
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUsersListHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
