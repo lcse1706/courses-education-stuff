905b76f340e0a177c2df8e1ba36396a6a0700db1
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-09 11:49:02 +0100

    React Portals

--- React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/components/UI/ErrorModal.js
+++ React/MaxSchwarzmüller-Udemy/Practice-AddUser-Validation/src/components/UI/ErrorModal.js
@@ -4,23 +4,45 @@ import classes from './ErrorModal.module.css';
 
 import Card from './Card';
 import Button from './Button';
+import ReactDom from 'react-dom';
+
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.onSubmitError} />;
+};
+
+const Modal = props => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p className={classes.content}>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onSubmitError}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = props => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onSubmitError} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div>
-          <p className={classes.content}>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onSubmitError}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDom.createPortal(
+        <Backdrop onSubmitError={props.onSubmitError} />,
+        document.getElementById('backdrop-root')
+      )}
+
+      {ReactDom.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onSubmitError={props.onSubmitError}
+        />,
+        document.getElementById('modal-root')
+      )}
+    </>
   );
 };
 
