3df6cef670208a3fb71fe1133755b3e66af9aeef
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-11 11:27:30 +0100

    usereducer()

--- React/MaxSchwarzmüller-Udemy/Usereducer()-starting-project/src/components/Login/Login.js
+++ React/MaxSchwarzmüller-Udemy/Usereducer()-starting-project/src/components/Login/Login.js
@@ -27,8 +27,8 @@ const passwordReducer = (state, action) => {
 const Login = props => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -87,7 +87,7 @@ const Login = props => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor='password'>Password</label>
