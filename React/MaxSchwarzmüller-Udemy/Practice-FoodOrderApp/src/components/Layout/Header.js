a48a5d68e1eb3efbcf01e3ea7e3e1ddb21a935fa
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-16 05:01:33 +0100

    Header + CartButton

---
+++ React/MaxSchwarzmüller-Udemy/Practice-FoodOrderApp/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import classes from './Header.module.css';
+import headerImage from '../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>RactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={headerImage} alt='A table full of food' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
