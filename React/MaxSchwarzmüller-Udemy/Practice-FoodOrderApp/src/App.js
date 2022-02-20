f9b373b008e477f80cff06ee1135c77fbad9bffc
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-02-20 20:34:21 +0100

    useReducer() - working on adding items to Cart

--- React/MaxSchwarzmüller-Udemy/Practice-FoodOrderApp/src/App.js
+++ React/MaxSchwarzmüller-Udemy/Practice-FoodOrderApp/src/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartisShown] = useState(false);
@@ -15,11 +16,11 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <Meals />
-    </Fragment>
+    </CartProvider>
   );
 }
 
