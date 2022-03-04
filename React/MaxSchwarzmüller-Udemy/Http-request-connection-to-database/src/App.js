a6b95c74546c4892c63426a5e7930bdbee30b139
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-03-04 06:26:24 +0100

    fetch()

--- React/MaxSchwarzmüller-Udemy/Http-request-connection-to-database/src/App.js
+++ React/MaxSchwarzmüller-Udemy/Http-request-connection-to-database/src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () => {
+    fetch('https://swapi.dev/api/films')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const movieDataFromApi = data.results.map(data => {
+          return {
+            id: data.episode_id,
+            title: data.title,
+            openingText: data.opening_crawl,
+            realeaseDate: data.realease_date,
+          };
+        });
+        setMovies(movieDataFromApi);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
