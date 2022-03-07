1d311e963c354a4e8dbfd32db30eea46b5da9e36
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-03-07 05:44:48 +0100

    Get/Post requests using Firebase

--- React/MaxSchwarzmüller-Udemy/Http-request-connection-to-database/src/App.js
+++ React/MaxSchwarzmüller-Udemy/Http-request-connection-to-database/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,26 +14,54 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(
+        'https://test-3abc0-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+      );
 
       if (!response.ok) {
         throw new Error('Something went wrong !');
       }
 
       const data = await response.json();
-      const movieDataFromApi = data.results.map(data => {
-        return {
-          id: data.episode_id,
-          title: data.title,
-          openingText: data.opening_crawl,
-          realeaseDate: data.realease_date,
-        };
-      });
-      setMovies(movieDataFromApi);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const movieDataFromApi = data.results.map(data => {
+      //   return {
+      //     id: data.episode_id,
+      //     title: data.title,
+      //     openingText: data.opening_crawl,
+      //     realeaseDate: data.realease_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setLoading(false);
+  }
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      'https://test-3abc0-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   // function fetchMoviesHandler() {
@@ -54,6 +83,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
