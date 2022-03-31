96461b9eb747ead8d1978045a07c656d9b9c2f66
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-03-31 05:58:02 +0200

    Different ways to get input value

--- React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/SimpleInput.js
+++ React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/SimpleInput.js
@@ -1,11 +1,32 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from 'react';
+
+const SimpleInput = props => {
+  const [enteredName, setEnteredName] = useState('');
+  const refName = useRef();
+
+  const changeNameHandler = e => {
+    setEnteredName(e.target.value);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    console.log(enteredName);
+    console.log(refName.current.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={changeNameHandler}
+          ref={refName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
