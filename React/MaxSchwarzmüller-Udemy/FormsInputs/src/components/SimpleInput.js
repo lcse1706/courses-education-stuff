b69bb77fdee20be42fc6033a3ac51447f48a4bea
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-03-31 11:06:14 +0200

    all possibilities of input validation

--- React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/SimpleInput.js
+++ React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/SimpleInput.js
@@ -2,28 +2,42 @@ import { useRef, useState } from 'react';
 
 const SimpleInput = props => {
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameisValid, setEnteredNameValid] = useState(true);
+  const [enteredNameisValid, setEnteredNameValid] = useState(false);
+  const [inputIsTouched, setInputIsTouched] = useState(false);
   const refName = useRef();
 
   const changeNameHandler = e => {
     setEnteredName(e.target.value);
+
+    if (e.target.value.trim() !== '') {
+      setEnteredNameValid(true);
+    }
+  };
+
+  const nameInputBlurHandler = e => {
+    setInputIsTouched(true);
+    if (enteredName.trim() === '') {
+      setEnteredNameValid(false);
+    }
   };
 
   const submitHandler = e => {
     e.preventDefault();
+    setInputIsTouched(true);
 
     if (enteredName.trim() === '') {
       setEnteredNameValid(false);
       return;
     }
-
     console.log(enteredName);
     console.log(refName.current.value);
   };
 
-  const nameInputClasses = enteredNameisValid
-    ? 'form-control'
-    : 'form-control invalid';
+  const nameInputIsInvalid = !enteredNameisValid && inputIsTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
 
   return (
     <form onSubmit={submitHandler}>
@@ -33,9 +47,10 @@ const SimpleInput = props => {
           type='text'
           id='name'
           onChange={changeNameHandler}
+          onBlur={nameInputBlurHandler}
           ref={refName}
         />
-        {!enteredNameisValid && (
+        {nameInputIsInvalid && (
           <p className='error-text'>Name field cannot be empty.</p>
         )}
       </div>
