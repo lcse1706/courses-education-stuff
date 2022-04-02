dfb80ca9ac0dab6eaf85cb212a90396c8a6dde62
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-04-02 07:06:28 +0200

    Practise part, setting up new form

--- React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/BasicForm.js
+++ React/MaxSchwarzmüller-Udemy/FormsInputs/src/components/BasicForm.js
@@ -1,10 +1,42 @@
-const BasicForm = (props) => {
+import { useState } from 'react';
+
+const BasicForm = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const changeNameHandler = e => {
+    setEnteredValue(e.target.value);
+  };
+
+  const blurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const submitFormHandler = e => {
+    e.preventDefault();
+    console.log(enteredValue);
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+
+  const inputIsInValid = enteredValue.trim() === '' && isTouched;
+
+  const nameInputClassHandler = inputIsInValid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClassHandler}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            onChange={changeNameHandler}
+            onBlur={blurHandler}
+            value={enteredValue}
+          />
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
