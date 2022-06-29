295862269e6db7493a35165bfc45c85adbfe3f82
Lukas Czarniecki <lcse.2001@gmail.com>
2022-06-29 15:13:40 +0200

    function types

--- TypeScript-MaxSchwarzmüller-Udemy/Starting-project/app.ts
+++ TypeScript-MaxSchwarzmüller-Udemy/Starting-project/app.ts
@@ -1,32 +1,17 @@
-type Combine = number | string;
-
-function combine(
-  input1: Combine,
-  input2: Combine,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Results: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+printResult(add(5, 12));
 
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
