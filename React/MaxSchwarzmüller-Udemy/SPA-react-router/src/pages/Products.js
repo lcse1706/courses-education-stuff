86f955740941ea80df7e7d402ad5637ddda15e04
Lukas Czarniecki <lcse.2001@gmail.com>
2022-10-05 12:13:23 +0200

    Link NavLink params

--- React/MaxSchwarzmüller-Udemy/SPA-react-router/src/pages/Products.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router/src/pages/Products.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
 const Products = () => {
-  return <h1>Products Page</h1>;
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/book">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/blanket">A blanket</Link>
+        </li>
+        <li>
+          <Link to="/products/onlinecourse">An Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
