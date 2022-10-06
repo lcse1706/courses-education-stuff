4dd8e9da9338a08cf9ac1dd663ef96c6b67a5163
Lukas Czarniecki <lcse.2001@gmail.com>
2022-10-06 14:06:27 +0200

    working on routing

--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
@@ -1,5 +1,18 @@
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const param = useParams();
+  return (
+    <section>
+      <h1>Quote Detail Page</h1>
+      <p>{param.quoteId}</p>
+      <Route path={`/quotes/${param.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </section>
+  );
 };
 
 export default QuoteDetail;
