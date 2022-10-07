c14236222651e342eb290be66117907dd80afb79
Lukas Czarniecki <lcse.2001@gmail.com>
2022-10-07 10:16:17 +0200

    all quotes, quotes forms, quotes details

--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/AllQuotes.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const RANDOM_QUOTES = [
+  { id: "q1", author: "Lukas", text: "Learning react is fun !" },
+  { id: "q2", author: "SMB", text: "Comparasion is thief of the joy !" },
+  { id: "q3", author: "SMB2", text: "Creating is  expanding !" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={RANDOM_QUOTES} />;
 };
 
 export default AllQuotes;
--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { useParams, Route } from "react-router-dom";
-
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-const QuoteDetail = () => {
-  const param = useParams();
+const RANDOM_QUOTES = [
+  { id: "q1", author: "Lukas", text: "Learning react is fun !" },
+  { id: "q2", author: "SMB", text: "Comparasion is thief of the joy !" },
+  { id: "q3", author: "SMB2", text: "Creating is  expanding !" },
+];
+
+const QuoteDetail = (props) => {
+  const params = useParams();
+  const quote = RANDOM_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+
   return (
     <section>
-      <h1>Quote Detail Page</h1>
-      <p>{param.quoteId}</p>
-      <Route path={`/quotes/${param.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </section>
