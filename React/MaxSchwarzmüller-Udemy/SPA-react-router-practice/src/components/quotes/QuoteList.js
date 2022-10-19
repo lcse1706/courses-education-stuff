50d5f78335d13cd39a343b35b9b97aa24df14ded
Lukas Czarniecki <lcse.2001@gmail.com>
2022-10-19 12:06:31 +0200

    add comments

--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/components/quotes/QuoteList.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/components/quotes/QuoteList.js
@@ -1,8 +1,8 @@
-import { Fragment } from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
-import QuoteItem from "./QuoteItem";
-import classes from "./QuoteList.module.css";
+import QuoteItem from './QuoteItem';
+import classes from './QuoteList.module.css';
 
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
@@ -14,30 +14,30 @@ const sortQuotes = (quotes, ascending) => {
   });
 };
 
-const QuoteList = (props) => {
+const QuoteList = props => {
   const history = useHistory();
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
 
   //We can use simply location.search and check if location.search === '?sort=asc'
-  const isSortingAscending = queryParams.get("sort") === "asc";
+  const isSortingAscending = queryParams.get('sort') === 'asc';
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
   };
 
   return (
     <Fragment>
       <div className={classes.sorting}>
         <button onClick={changeSortingHandler}>
-          Sort {isSortingAscending ? "Descending" : "Ascending"}
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
         </button>
       </div>
       <ul className={classes.list}>
-        {sortedQuotes.map((quote) => (
+        {sortedQuotes.map(quote => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/QuoteDetail.js
@@ -1,16 +1,16 @@
-import { useParams, Route } from "react-router-dom";
-import HighlightedQuote from "../components/quotes/HighlightedQuote";
-import Comments from "../components/comments/Comments";
+import { useParams, Route, Link } from 'react-router-dom';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
 
 const RANDOM_QUOTES = [
-  { id: "q1", author: "Lukas", text: "Learning react is fun !" },
-  { id: "q2", author: "SMB", text: "Comparasion is thief of the joy !" },
-  { id: "q3", author: "SMB2", text: "Creating is  expanding !" },
+  { id: 'q1', author: 'Lukas', text: 'Learning react is fun !' },
+  { id: 'q2', author: 'SMB', text: 'Comparasion is thief of the joy !' },
+  { id: 'q3', author: 'SMB2', text: 'Creating is  expanding !' },
 ];
 
-const QuoteDetail = (props) => {
+const QuoteDetail = props => {
   const params = useParams();
-  const quote = RANDOM_QUOTES.find((quote) => quote.id === params.quoteId);
+  const quote = RANDOM_QUOTES.find(quote => quote.id === params.quoteId);
 
   if (!quote) {
     return <p>No quote found</p>;
@@ -19,6 +19,14 @@ const QuoteDetail = (props) => {
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
