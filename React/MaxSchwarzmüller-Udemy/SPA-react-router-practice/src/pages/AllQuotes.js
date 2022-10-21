747ced33ee2aebacc0065e5258b398c8949c57b4
Lukas Czarniecki <lcse.2001@gmail.com>
2022-10-21 12:11:46 +0200

    AllQuotes.js - Connecting with firebase

--- React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/AllQuotes.js
+++ React/MaxSchwarzmüller-Udemy/SPA-react-router-practice/src/pages/AllQuotes.js
@@ -1,13 +1,39 @@
-import QuoteList from "../components/quotes/QuoteList";
-
-const RANDOM_QUOTES = [
-  { id: "q1", author: "Lukas", text: "Learning react is fun !" },
-  { id: "q2", author: "SMB", text: "Comparasion is thief of the joy !" },
-  { id: "q3", author: "SMB2", text: "Creating is  expanding !" },
-];
+import { useEffect } from 'react';
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 const AllQuotes = () => {
-  return <QuoteList quotes={RANDOM_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
