a1312aee8b61132903b5a31fe0b2d21cdd6e3745
Lukas Czarniecki <lcse.2001@gmail.com>
2023-01-18 21:53:33 +0100

    rebulding ts folder

--- wesbos-JS/scroll-events/scroll-to-accept.js
+++ wesbos-JS/scroll-events/scroll-to-accept.js
@@ -3,23 +3,21 @@ const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
 const button = document.querySelector('.accept');
 
-
 function observerCallback(payload) {
-    if(payload[0].intersectionRatio === 1){ // payload[0] becouse we can observe many things on the page
-        button.disabled = false;
-        //stop observing the button
-        observer.unobserve(terms.lastElementChild);
-    }
-    // else{
-    //     button.disabled = true;
-    // }
-     
+  if (payload[0].intersectionRatio === 1) {
+    // payload[0] becouse we can observe many things on the page
+    button.disabled = false;
+    //stop observing the button
+    observer.unobserve(terms.lastElementChild);
+  }
+  // else{
+  //     button.disabled = true;
+  // }
 }
 const observer = new IntersectionObserver(observerCallback, {
-    root: terms,
-    threshold: 1,
+  root: terms,
+  threshold: 1,
 });
 
 // observer.observe(watch);
 observer.observe(terms.lastElementChild);
-
