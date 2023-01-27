5da1b3b7f049edc02829d6f8c7bc198c91e8ad39
Lukas Czarniecki <lcse.2001@gmail.com>
2023-01-27 23:25:46 +0100

    component interface

---
+++ TypeScript/Stephen Grider/tsapp/src/props/Parent.tsx
@@ -0,0 +1,11 @@
+import { Child } from './Child'
+
+
+
+const Parent = () =>{
+  return <Child color="red" onClick={() => { console.log('Clicked')}} />
+     Hi There. 
+  </Child>
+}
+
+export default Parent;
