6a7e8e713eb233db13c8f04bcbec038c32d784c4
Lukas Czarniecki <lcse.2001@gmail.com>
2023-01-28 00:21:46 +0100

    toDo and search throught the list

--- TypeScript/Stephen Grider/tsapp/src/App.tsx
+++ TypeScript/Stephen Grider/tsapp/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+// import GuestList from './state/GueastList';
+import UserSearch from './state/UserSearch';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hi there</h1>
+     {/* <GuestList/> */}
+
+     <UserSearch/>
     </div>
   );
 }
--- TypeScript/Stephen Grider/tsapp/src/props/Parent.tsx
+++ TypeScript/Stephen Grider/tsapp/src/props/Parent.tsx
@@ -4,8 +4,6 @@ import { Child } from './Child'
 
 const Parent = () =>{
   return <Child color="red" onClick={() => { console.log('Clicked')}} />
-     Hi There. 
-  </Child>
 }
 
 export default Parent;
