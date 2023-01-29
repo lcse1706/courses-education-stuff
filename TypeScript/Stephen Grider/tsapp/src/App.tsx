b6269f84c8c144a781044a977d938d5ff2335e53
Lukas Czarniecki <lcse.2001@gmail.com>
2023-01-29 18:03:34 +0100

    event components in TS

--- TypeScript/Stephen Grider/tsapp/src/App.tsx
+++ TypeScript/Stephen Grider/tsapp/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import EventComponent from './events/EventComponent';
 // import GuestList from './state/GueastList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
 
 function App() {
   return (
     <div className="App">
      {/* <GuestList/> */}
-
-     <UserSearch/>
+     {/* <UserSearch/> */}
+     <EventComponent />
     </div>
   );
 }
