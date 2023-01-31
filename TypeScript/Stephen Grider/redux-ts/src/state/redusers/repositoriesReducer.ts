d71af48620c844329a877536ab3a3a71edb16042
Lukas Czarniecki <lcse.2001@gmail.com>
2023-01-31 22:46:50 +0100

    rename file

---
+++ TypeScript/Stephen Grider/redux-ts/src/state/redusers/repositoriesReducer.ts
@@ -0,0 +1,24 @@
+import {ActionType} from '../action-types'
+import {Action} from '../actions'
+
+interface RepositoriesStates {
+  loading: boolean;
+  error: string | null;
+  data: string[]
+}
+
+const reducer = (state: RepositoriesStates, action: Action
+  ): RepositoriesStates =>{
+    switch (action.type) {
+      case ActionType.SEARCH_REPOSITORIES:
+        return {loading: true, error: null, data: []};
+      case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+        return {loading: false, error: null, data: action.payload};
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
+        return {loading: false, error: action.payload, data: []};
+      default:
+        return state;
+    }
+}
+
+export default reducer;
