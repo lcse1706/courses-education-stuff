4913205035853d3d540e02a02b1cd977712891e5
Lukas Czarniecki <lcse.2001@gmail.com>
2023-02-02 19:51:07 +0100

    taking out needed data with useSelector hook - finaly useTypedSelecetor

--- TypeScript/Stephen Grider/redux-ts/src/components/RepositoriesList.tsx
+++ TypeScript/Stephen Grider/redux-ts/src/components/RepositoriesList.tsx
@@ -2,20 +2,25 @@ import { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { actionCreators } from '../state';
 import { useAction } from '../hooks/useActions';
-
+// import { useSelector } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList:React.FC = () => {
 const [term, setTerm] = useState('');
 // const dispatch = useDispatch();
-
 const {searchRepositories} = useAction();
+
+//Aby ts dobrze okreslil typ, musielismy exportowac typ z reducers/index.ts, nastepnie wyeksportowac go ze state/index.ts, na koncu stworzyc hooks/useTypedSelector.ts
+
+const {data, error, loading} = useTypedSelector((state) => state.repositories)
+
 
 const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     
     // dispatch(actionCreators.searchRepositories(term) as any);
 
-    searchRepositories(term)
+    searchRepositories(term);
 }
 
   return (
@@ -24,6 +29,10 @@ const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
       <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>Loading...</h3>}
+    {!error && !loading && 
+    data.map(item => <div key={item}>{item}</div>)}
   </div>
  )
 }
