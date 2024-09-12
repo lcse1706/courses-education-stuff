2ec4deeb1611a694429fb24a102e4ec825e4f4f3
lukczar1706 <lcse.2001@gmail.com>
2024-09-12 16:36:54 +0200

    update: type fitting

--- nodeJS-postgreSQL/backend-typeORM/src/entity/User.ts
+++ nodeJS-postgreSQL/backend-typeORM/src/entity/User.ts
@@ -1,25 +1,16 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({
-    length: 50,
-    default: '',
-  })
-  name: string = '';
+  @Column({ length: 50 })
+  name!: string;
 
-  @Column({
-    default: 0,
-  })
-  age: number = 0;
+  @Column({ type: 'int', nullable: false })
+  age!: number;
 
-  @Column({
-    type: 'varchar',
-    length: 6,
-    default: 'Male',
-  })
-  gender: 'Male' | 'Female' = 'Male';
+  @Column({ type: 'varchar', default: 'Male' })
+  gender!: 'Male' | 'Female';
 }
